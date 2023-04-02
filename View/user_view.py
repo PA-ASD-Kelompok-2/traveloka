8096158c08c1a4214d451f4cf491b1c0a26b824a
Fauzan Gifari <fauzan.gifari30@gmail.com>
2023-04-02 21:27:53 +0800

    Add some feature on user view

--- View/user_view.py
+++ View/user_view.py
@@ -1,23 +1,58 @@
+from View import flight_view as fv
 from Controller import flight_controller as fc
-class MenuUser:
-    def __init__(self):     
-        print("==================================")
-        print("|            M E N U             |")
-        print("==================================")
-        print("|-----> Menu yang tersedia <-----|")
-        print("|                                |")
-        print("|    1. Tampilkan Profil         |")
-        print("|    2. Cari Tiket Pesawat       |")
-        print("|    3. Tampilkan Tiket Pesawat  |")
-        print("|    4. Tambah Saldo             |")
-        print("|                                |")
-        print("==================================")
-        opsi = str(input("Tentukan opsi anda (1/2/3/4/5): "))
-        if opsi == 1:
-            pass
-        if opsi == 2:
-            pass
-        if opsi == 3:
-            pass
-        if opsi == 4:
-            pass
+from Controller import auth_controller as auth
+import main
+
+
+class UserView:
+
+    def __init__(self):
+        self.flight = fc.Flight()
+        self.auth = auth.User()
+        self.view = fv.LinkedList()
+
+    def menuUser(self):
+        while True:
+            print("==================================")
+            print("|            M E N U             |")
+            print("==================================")
+            print("|-----> Menu yang tersedia <-----|")
+            print("|                                |")
+            print("|    1. Pesan Tiket Pesawat      |")
+            print("|    2. Booking Hotel            |")
+            print("|    3. Sign Out                 |")
+            print("|                                |")
+            print("==================================")
+            opsi = str(input("Tentukan opsi anda (1/2/3/4/5): "))
+
+            if opsi == '1':
+                pass
+            elif opsi == '2':
+                pass
+            elif opsi == '3':
+                main.MenuUtama().run()
+            else:
+                print("Opsi tidak tersedia!")
+
+    def userFlight(self):
+        while True:
+            print("==================================")
+            print("|            M E N U             |")
+            print("==================================")
+            print("|-----> Menu yang tersedia <-----|")
+            print("|                                |")
+            print("|    1. Cari Tiket Pesawat       |")
+            print("|    2. Pemesanan Tiket          |")
+            print("|    3. Kembali                  |")
+            print("|                                |")
+            print("==================================")
+            opsi = str(input("Tentukan opsi anda (1/2/3/4/5): "))
+
+            if opsi == '1':
+                pass
+            elif opsi == '2':
+                pass
+            elif opsi == '3':
+                UserView.menuUser(self)
+            else:
+                print("Opsi tidak tersedia!")
